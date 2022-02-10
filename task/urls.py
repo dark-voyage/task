d7184653032df8da8444bfe062833b654e8ef570
genemators <itstoolongname@gmail.com>
2022-02-10 22:39:17 +0500

    fixing new bugs

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,11 @@
 from urllib import request
 from django.urls import path
-from .views import AdView, AdListView, ImageCreateView, ImageView
+from .views import AdListView, ImageListView, AdRUDView, ImageRUDView
 
 
 urlpatterns = [
     path('ads/', AdListView.as_view()),
-    path('ads/<int:pk>/', AdView.as_view()),
-    path('ads/img/', ImageCreateView.as_view()),
-    path('ads/img/<int:pk>/', ImageView.as_view()),
+    path('ads/<int:pk>/', AdRUDView.as_view()),
+    path('ads/img/', ImageListView.as_view()),
+    path('ads/img/<int:pk>/', ImageRUDView.as_view()),
 ]
--- task/views.py
+++ task/views.py
@@ -1,6 +1,5 @@
 from .models import Ad, Image
 from .serializers import AdSerializer, ImageSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
@@ -8,39 +7,14 @@ class AdListView(generics.ListCreateAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
 
-
-class AdView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class AdRUDView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-class ImageCreateView(generics.ListCreateAPIView):
+class ImageListView(generics.ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
 
-class ImageView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class ImageRUDView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
