3bce4848226c63ba49ac22d3bdebe806248a7b2a
genemators <itstoolongname@gmail.com>
2022-02-10 16:23:41 +0500

    added models

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
+from operator import mod
+from statistics import mode
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    name = models.CharField(max_length=200)
+    descriptiom = models.TextField(max_length=1000)
+
+class Images(models.Model):
+    ad = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    link = models.URLField(max_length=200)
