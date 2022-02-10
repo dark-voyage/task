cca865bf72b37c8c025a6b4b4a9fc0ccd1bec048
genemators <itstoolongname@gmail.com>
2022-02-10 16:31:27 +0500

    changed models

--- task/models.py
+++ task/models.py
@@ -4,7 +4,12 @@ from django.db import models
 
 class Advertisement(models.Model):
     name = models.CharField(max_length=200)
-    descriptiom = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    main_image = models.URLField(max_length=200)
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
 class Images(models.Model):
     ad = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
