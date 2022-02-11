87a85c3c8fc79fcf6099bfd9ac0d09462b4baf5b
genemators <itstoolongname@gmail.com>
2022-02-11 19:55:56 +0500

    my first django test

--- task/tests.py
+++ task/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Ad, Image
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Ad.objects.create(name="Van Darkholme", description="Full Dungeon Master",  price=300)
+        Image.objects.create(ad=Ad.objects.get(name="Van Darkholme"), link="https://docs.djangoproject.com/en/4.0/topics/testing/overview/")
+
+    def test_ad_has_an_image(self):
+        test_ad = Ad.objects.get(name="Van Darkholme")
+        test_image = Image.objects.get(ad=test_ad)
+        self.assertEqual(test_image.ad, test_ad)
+       
