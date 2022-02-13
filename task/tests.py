b254283fe17787c3e5a2a5c01c06d186b59670bf
genemators <itstoolongname@gmail.com>
2022-02-13 21:32:23 +0500

    Revert "300bucks"
    This reverts commit b72de78867c80b48e41755da498926d69a7e9714.

--- task/tests.py
+++ task/tests.py
@@ -3,11 +3,11 @@ from django.test import TestCase
 
 class AnimalTestCase(TestCase):
     def setUp(self):
-        self.ad = Ad.objects.create(name="Van Darkholme", description="Full Dungeon Master",  price=300)
-        Image.objects.create(ad=self.ad, link="https://docs.djangoproject.com/en/4.0/topics/testing/overview/")
+        Ad.objects.create(name="Van Darkholme", description="Full Dungeon Master",  price=300)
+        Image.objects.create(ad=Ad.objects.get(name="Van Darkholme"), link="https://docs.djangoproject.com/en/4.0/topics/testing/overview/")
 
     def test_ad_has_an_image(self):
-        test_ad = self.ad
+        test_ad = Ad.objects.get(name="Van Darkholme")
         test_image = Image.objects.get(ad=test_ad)
         self.assertEqual(test_image.ad, test_ad)
        
